c7d52bc2227361b91e916302b38c52d46a36324f
Glitch (thundering-turtle) <none>
2019-02-18 00:59:56 +0000

    🎻🏵 Checkpoint ./package.json:377298/40 ./server.js:377298/1383

--- server.js
+++ server.js
@@ -1,39 +1,27 @@
-// server.js
-// where your node app starts
-
-// init project
 const express = require('express')
 const request = require('request')
 const app = express()
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
-
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'))
-
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + '/views/index.html')
+  response.setHeader('Content-Type: text/plain');
+  response.send("OK");
+});
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get("/forecast", (request, response) => {
+  request.get({
+    uri: process.env.FORECAST_API,
+    qs: {
+      q: "dracut,us"
+    }
+  });
 })
 
-// Simple in-memory store
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-]
-
-app.get("/dreams", (request, response) => {
-})
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", (request, response) => {
-  dreams.push(request.query.dream)
-  response.sendStatus(200)
-})
-
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`)
-})
