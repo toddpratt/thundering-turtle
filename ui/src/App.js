69ae6601f2688831442de2e002e2d42987d7d2b5
Todd A. Pratt <tpratt@protonmail.com>
2019-02-20 06:33:12 -0500

    Adds charts

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import {Container, Jumbotron} from 'reactstrap';
-import chartjs from 'react-chartjs';
+import { Container, Jumbotron } from 'reactstrap';
 import logo from './logo.svg';
 import './App.css';
+import ReactChartkick, { LineChart } from 'react-chartkick'
+import Chart from 'chart.js'
+
 
 const Banner = props => (
     <Jumbotron className="page-header panel panel-default">
@@ -10,37 +12,89 @@ const Banner = props => (
     </Jumbotron>
 );
 
-class TemperatureChart extends Component {
+class BaseChart extends Component {
     constructor(props) {
-        super();
+        super(props);
         this.state = {
-            data: {
-                labels: [],
-                datasets: [{
-                    label: '',
-                    data: [],
-                    backgroundColor: [],
-                    borderColor: [],
-                    borderWidth: 1
-                }]
-            }
         };
     }
 
+    getValue(val) {
+    }
+
+    getData() {
+        if (!this.props.data) return null;
+
+        const data = [];
+        const temps = this.props.data.list;
+        temps.forEach(val => {
+            const key = val.dt_txt;
+            const value = this.getValue(val);
+            data.push([key, value])
+        });
+
+        return data;
+    }
+
+}
+
+class TemperatureChart extends BaseChart {
     render() {
         return (
-            <chartjs.Line data={this.props.data} />
+            <div>
+                <div>Temperature</div>
+                <LineChart data={this.getData()}
+                    height="600px" min={null} suffix="&#x2109;" />
+            </div>
         );
+    }
+
+    getValue(val) {
+        return val.main.temp;
     }
 }
 
-class App extends Component {
-    constructor(props) {
-        super();
-        this.state = {};
+class HumidityChart extends BaseChart {
+    render() {
+        return (
+            <div>
+                <div>Humidity</div>
+                <LineChart data={this.getData()}
+                    height="600px" min={null} suffix="%" />
+            </div>
+        );
     }
 
-    componentDidMount() {
+    getValue(val) {
+        return val.main.humidity;
+    }
+}
+
+class WindSpeedChart extends BaseChart {
+    render() {
+        return (
+            <div>
+                <div>Wind Speed</div>
+                <LineChart data={this.getData()}
+                    height="600px" min={null} suffix="MPH" />
+            </div>
+        );
+    }
+
+    getValue(val) {
+        return val.wind.speed;
+    }
+}
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        setInterval(this.updateForecast, 10000)
+    }
+
+    updateForecast() {
         fetch("https://thundering-turtle.glitch.me/forecast?city=Dracut,ma,us")
             .then(res => res.json())
             .then(json => {
@@ -51,11 +105,17 @@ class App extends Component {
             });
     }
 
+    componentDidMount() {
+        this.updateForecast();
+    }
+
     render() {
         return (
             <div className="App">
                 <Banner />
                 <TemperatureChart data={this.state.forecast} />
+                <HumidityChart data={this.state.forecast} />
+                <WindSpeedChart data={this.state.forecast} />
             </div>
         );
     }
