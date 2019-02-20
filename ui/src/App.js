5c289b5f0b2bdfcb8aab1f4bf07bd88813e0d509
Todd A. Pratt <tpratt@protonmail.com>
2019-02-19 19:29:49 -0500

    build

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
+import {Container, Jumbotron} from 'reactstrap';
+import chartjs from 'react-chartjs';
 import logo from './logo.svg';
 import './App.css';
 
+const Banner = props => (
+    <Jumbotron className="page-header panel panel-default">
+        <h2>Dracut Weather</h2>
+    </Jumbotron>
+);
+
+class TemperatureChart extends Component {
+    constructor(props) {
+        super();
+        this.state = {
+            data: {
+                labels: [],
+                datasets: [{
+                    label: '',
+                    data: [],
+                    backgroundColor: [],
+                    borderColor: [],
+                    borderWidth: 1
+                }]
+            }
+        };
+    }
+
+    render() {
+        return (
+            <chartjs.Line data={this.props.data} />
+        );
+    }
+}
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    constructor(props) {
+        super();
+        this.state = {};
+    }
+
+    componentDidMount() {
+        fetch("https://thundering-turtle.glitch.me/forecast?city=Dracut,ma,us")
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    status: json.status,
+                    forecast: json.data
+                });
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Banner />
+                <TemperatureChart data={this.state.forecast} />
+            </div>
+        );
+    }
 }
 
 export default App;
