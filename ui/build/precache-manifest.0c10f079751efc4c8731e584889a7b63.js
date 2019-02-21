d5579b87c903585593c56f03be21252b3654f0ed
Todd A. Pratt <tpratt@protonmail.com>
2019-02-20 20:55:47 -0500

    commit the built files.

---
+++ ui/build/precache-manifest.0c10f079751efc4c8731e584889a7b63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "179e4d283b451b59e9f9",
+    "url": "/static/js/main.179e4d28.chunk.js"
+  },
+  {
+    "revision": "5237c7879d3bd7653816",
+    "url": "/static/js/2.5237c787.chunk.js"
+  },
+  {
+    "revision": "179e4d283b451b59e9f9",
+    "url": "/static/css/main.7d9c4fab.chunk.css"
+  },
+  {
+    "revision": "fa8b228ec7b7b0112990639c22ab4678",
+    "url": "/index.html"
+  }
+];
