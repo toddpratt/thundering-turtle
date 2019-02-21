b8b2306e73cfe192d3b07134be824df4a0c809ec
Todd A. Pratt <tpratt@protonmail.com>
2019-02-20 20:02:48 -0500

    copy from old project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const request = require('request-promise-native');
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.use(express.static('/ui/build/'))
+
+app.get("/forecast", (req, res) => {
+  res.header('Content-Type: application/json');
+  request.get({
+    uri: process.env.FORECAST_URI,
+    qs: {
+      q: req.query.city,
+      appid: process.env.API_KEY,
+      units: "imperial"
+    },
+    json: true
+  })
+  .then(result => {
+    res.send({
+      status: "OK",
+      data: result
+    });
+  })
+  .catch(err => {
+    res.send({
+      status: "ERROR",
+      error: err
+    })
+  });
+})
+
+app.listen(process.env.PORT);
---
+++ ui/src/App.js
@@ -0,0 +1,143 @@
+import React, { Component } from 'react';
+import { Container, Jumbotron } from 'reactstrap';
+import logo from './logo.svg';
+import './App.css';
+import ReactChartkick, { LineChart } from 'react-chartkick'
+import Chart from 'chart.js'
+
+
+class Banner extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    render() {
+        var bannerText = "Weather: New York...";
+
+        if (this.props.data) {
+            bannerText = "Weather: " + this.props.data.city.name;
+        };
+
+        console.log(this.props);
+        return (
+            <Jumbotron className="page-header panel panel-default">
+                <h2>{bannerText}</h2>
+            </Jumbotron>
+        );
+    }
+}
+
+class BaseChart extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    getData() {
+        if (!this.props.data) return null;
+
+        const data = [];
+        const temps = this.props.data.list;
+        temps.forEach(val => {
+            const key = val.dt_txt;
+            const value = this.getValue(val);
+            data.push([key, value])
+        });
+
+        return data;
+    }
+
+}
+
+class TemperatureChart extends BaseChart {
+    render() {
+        return (
+            <div className="panel panel-default chart-container">
+                <div className="panel-heading"><h3>Temperature</h3></div>
+                <div className="panel-body">
+                    <LineChart data={this.getData()}
+                        height="600px" min={null} suffix="&#x2109;" />
+                </div>
+            </div>
+        );
+    }
+
+    getValue(val) {
+        return val.main.temp;
+    }
+}
+
+class HumidityChart extends BaseChart {
+    render() {
+        return (
+            <div className="panel panel-default chart-container">
+                <div className="panel-heading"><h3>Humidity</h3></div>
+                <div className="panel-body">
+                    <LineChart data={this.getData()}
+                        height="600px" min={null} suffix="%" />
+                </div>
+            </div>
+        );
+    }
+
+    getValue(val) {
+        return val.main.humidity;
+    }
+}
+
+class WindSpeedChart extends BaseChart {
+    render() {
+        return (
+            <div className="panel panel-default chart-container">
+                <div className="panel-heading"><h3>Wind Speed</h3></div>
+                <div className="panel-body">
+                    <LineChart data={this.getData()}
+                        height="600px" min={null} suffix="MPH" />
+                </div>
+            </div>
+        );
+    }
+
+    getValue(val) {
+        return val.wind.speed;
+    }
+}
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.updateForecast = this.updateForecast.bind(this);
+    }
+
+    updateForecast() {
+        fetch("https://thundering-turtle.glitch.me/forecast?city=New+York,ny,us")
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    status: json.status,
+                    forecast: json.data
+                });
+            });
+    }
+
+    componentDidMount() {
+        this.updateForecast();
+        setInterval(this.updateForecast, 300000)
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Banner data={this.state.forecast} />
+                <TemperatureChart data={this.state.forecast} />
+                <HumidityChart data={this.state.forecast} />
+                <WindSpeedChart data={this.state.forecast} />
+            </div>
+        );
+    }
+}
+
+export default App;
