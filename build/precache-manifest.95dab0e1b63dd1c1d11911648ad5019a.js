3d27bfe1757823af70b33df7bad2ab4406dc49bb
Todd A. Pratt <tpratt@protonmail.com>
2019-02-27 20:07:12 -0500

    add the built application

---
+++ build/precache-manifest.95dab0e1b63dd1c1d11911648ad5019a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e9e1a8f7a958e078b5e9",
+    "url": "/static/js/main.e9e1a8f7.chunk.js"
+  },
+  {
+    "revision": "0e226348683d44ecd91b",
+    "url": "/static/js/2.0e226348.chunk.js"
+  },
+  {
+    "revision": "e9e1a8f7a958e078b5e9",
+    "url": "/static/css/main.10c7cdd9.chunk.css"
+  },
+  {
+    "revision": "9b4d4c5bd8fded3bfa987a795a1c9d41",
+    "url": "/index.html"
+  }
+];
